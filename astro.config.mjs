a869cba0605b1ae481fb7cf09d94ae5886ee5306
princesuwal <princesuwal@gmail.com>
2026-04-22 21:34:57 -0500

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://princesuwal.github.io',
+  integrations: [sitemap()]
+});
