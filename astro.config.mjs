e3ea4afc1d052b27449ce77b11e20ef7b101f776
princesuwal <princesuwal@gmail.com>
2026-04-23 22:14:24 -0500

    Added css and template pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
   site: 'https://princesuwal.github.io',
   integrations: [sitemap()]
 });
