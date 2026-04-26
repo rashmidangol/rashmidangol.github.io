ef1da88a2a59d5b73bb5047e387b143f8767c0be
Rashmi Dangol <dangolrashmi@tamu.edu>
2026-04-26 11:55:35 -0500

    rashmi init

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://princesuwal.github.io',
+  site: 'https://rashmidangol.github.io',
   integrations: [sitemap()]
 });
